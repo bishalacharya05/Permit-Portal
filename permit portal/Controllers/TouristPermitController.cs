0f7638b2e3be106587ba8a8272f742c849a7f05b
bishalacharya05 <bishalacharya05@gmail.com>
2025-12-16 09:26:20 +0545

    Add project files.

---
+++ permit portal/Controllers/TouristPermitController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using permit_portal.Models;
+namespace permit_portal.Controllers
+{
+    public class TouristPermitController : Controller
+    {
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+       
+    }
+}
