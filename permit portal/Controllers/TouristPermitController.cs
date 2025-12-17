2a16342de2f89b7b73bc33ae9ad4f85b711f9b0d
bishalacharya05 <bishalacharya05@gmail.com>
2025-12-17 19:05:12 +0545

    Added email service

--- permit portal/Controllers/TouristPermitController.cs
+++ permit portal/Controllers/TouristPermitController.cs
@@ -2,13 +2,14 @@
 using permit_portal.Models.DomainModel;
 using permit_portal.Models.ViewModel;
 using permit_portal.Data;
+using Microsoft.EntityFrameworkCore;
 namespace permit_portal.Controllers
 {
     public class TouristPermitController : Controller
     {
         private readonly ApplicationDbContext context;
         private readonly IEmailService emailService;
-        public TouristPermitController(ApplicationDbContext context,IEmailService emailService)
+        public TouristPermitController(ApplicationDbContext context, IEmailService emailService)
         {
             this.emailService = emailService;
             this.context = context;
@@ -21,7 +22,7 @@ namespace permit_portal.Controllers
         }
 
         [HttpPost]
-        public async Task< IActionResult> Apply(TouristPermitViewModel touristPermitViewModel)
+        public async Task<IActionResult> Apply(TouristPermitViewModel touristPermitViewModel)
         {
             if (ModelState.IsValid)
             {
@@ -41,7 +42,7 @@ namespace permit_portal.Controllers
                 context.TouristPermits.Add(touristPermit);
                 context.SaveChanges();
 
-               
+
             }
 
             // Send confirmation email
@@ -73,35 +74,83 @@ namespace permit_portal.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
+            //In this get part we get the id of the applicant  and store it to the variable
             var touristPermit = context.TouristPermits.Find(id);
-            return View(touristPermit);
+
+            if (touristPermit == null)
+            {
+                return NotFound();
+            }
+            // Mapping the data to the domain model to the view model so we can pass the data in the input field of the edit form
+            var viewModel = new TouristPermitViewModel
+            {
+                FullName = touristPermit.FullName,
+                Email = touristPermit.Email,
+                Nationality = touristPermit.Nationality,
+                ContactNumber = touristPermit.ContactNumber,
+                PermitLocation = touristPermit.PermitLocation,
+                VisitDate = touristPermit.VisitDate,
+                NumberOfPeople = touristPermit.NumberOfPeople,
+                Purpose = touristPermit.Purpose
+            };
+            // Passing the viewModel data to the view of the Edit View
+            return View(viewModel);
+
         }
 
         [HttpPost]
-        public IActionResult Edit(TouristPermit touristPermitViewModel,int id)
+        public IActionResult Edit(TouristPermitViewModel viewmodel)
         {
-            if(ModelState.IsValid)
+
+            if (!ModelState.IsValid)
             {
-                var touristPermit = new TouristPermit
-                {
-                    FullName = touristPermitViewModel.FullName,
-                    Email = touristPermitViewModel.Email,
-                    Nationality = touristPermitViewModel.Nationality,
-                    ContactNumber = touristPermitViewModel.ContactNumber,
-                    PermitLocation = touristPermitViewModel.PermitLocation,
-                    VisitDate = touristPermitViewModel.VisitDate,
-                    NumberOfPeople = touristPermitViewModel.NumberOfPeople,
-                    Purpose = touristPermitViewModel.Purpose
-                };
-                context.TouristPermits.Update(touristPermit);
+                return View(viewmodel);
+            }
+            var touristPermit = context.TouristPermits.Find(viewmodel.Id);
+
+            if (touristPermit == null)
+            {
+                return NotFound();
+            }
+
+            touristPermit.FullName = viewmodel.FullName;
+            touristPermit.Email = viewmodel.Email;
+            touristPermit.Nationality = viewmodel.Nationality;
+            touristPermit.ContactNumber = viewmodel.ContactNumber;
+            touristPermit.PermitLocation = viewmodel.PermitLocation;
+            touristPermit.VisitDate = viewmodel.VisitDate;
+            touristPermit.NumberOfPeople = viewmodel.NumberOfPeople;
+            touristPermit.Purpose = viewmodel.Purpose;
+
+
+            context.TouristPermits.Update(touristPermit);
+            context.SaveChanges();
+            return RedirectToAction("Details");
+
+
+        }
+        
+
+        [HttpPost]
+        public IActionResult Delete (int id)
+        {
+            var touristPermit = context.TouristPermits.Find(id);
+
+            if(touristPermit!=null)
+            {
+                context.TouristPermits.Remove(touristPermit);
                 context.SaveChanges();
-                return RedirectToAction("Apply");
-                
+                return RedirectToAction("Details");
 
             }
-            return View(touristPermitViewModel);
+
+            return RedirectToAction("Details");
+
+            
         }
 
-       
     }
+
+       
+    
 }
--- permit portal/Models/ViewModel/TouristPermitViewModel.cs
+++ permit portal/Models/ViewModel/TouristPermitViewModel.cs
@@ -4,6 +4,8 @@ namespace permit_portal.Models.ViewModel
 {
     public class TouristPermitViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
